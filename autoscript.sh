173dfc2687509c0e16e6d4878add853494fff4af
herowinb <herowinb@gmail.com>
2026-06-10 23:58:46 +0700

    Fix: yt-dlp not work with cookies
    Force reinstall yt-dlp from a fork to fix error "No video formats found!" when use `--live-from-start` with cookies.

--- autoscript.sh
+++ autoscript.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Auto install last version of youtube-dl and chat-downloader for docker image
-pip install --no-cache-dir --upgrade --quiet yt-dlp yt-dlp-ejs
+# Force reinstall yt-dlp from a fork to fix error "No video formats found!" when use --live-from-start with cookies.
+# Will remove this after the merge complete.
+# https://github.com/yt-dlp/yt-dlp/pull/16771
+# https://github.com/yt-dlp/yt-dlp/issues/15274
+pip install --no-cache-dir --upgrade --quiet "yt-dlp[pin] @ git+https://github.com/dreammu/yt-dlp@live-adaptive-formats" yt-dlp-ejs
 
 # Run main script
 bash /usr/src/app/config/auto.sh
